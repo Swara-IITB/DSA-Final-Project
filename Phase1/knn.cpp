ebcb363e275ac7d0740799e3d3a993290e74d665
PLP1008 <hydraspace9@gmail.com>
2025-11-03 19:03:03 +0530

    Added knn.hpp and knn.cpp

---
+++ Phase1/knn.cpp
@@ -0,0 +1,63 @@
+#include "knn.hpp"
+std::vector<ll> euclidean(const Graph &g, ll id, std::string poi, ll k, double lat, double lon){
+    std::vector<std::pair<double,ll>> list;
+    for(auto n : g.nodes){
+        if(std::find(n.pois.begin(),n.pois.end(),poi)!=n.pois.end()){
+            double dist = std::sqrt((n.lat - lat)*(n.lat - lat) + (n.lon-lon)*(n.lon-lon));
+            list.push_back({dist,n.id});
+        }
+    }
+    std::sort(list.begin(),list.end());
+    std::vector<ll>ans;
+    int x = (list.size()<k)? list.size() : k;
+    for(int i=0;i<x;i++){
+        ans.push_back(list[i].second);
+    }
+    return ans;
+}
+
+ll nn(const Graph &g, double lat, double lon){
+    std::vector<std::pair<double,ll>> list;
+    for(auto n: g.nodes){
+        double dist = std::sqrt((n.lat - lat)*(n.lat - lat) + (n.lon-lon)*(n.lon-lon));
+        list.push_back({dist,n.id});
+    }
+    std::sort(list.begin(),list.end());
+    return list[0].second;
+}
+
+std::vector<ll>shortest_path(const Graph &g,ll id, std::string poi, ll k, double lat, double lon){
+    ll nn_id = nn(g,lat,lon);
+    std::vector<double> sp(g.adj.size(),__LONG_LONG_MAX__);
+    sp[nn_id]=0;
+    std::vector<bool> visit(g.adj.size(),false);
+    std::priority_queue<std::pair<double,ll>, std::vector<std::pair<double,ll>>, std::greater<std::pair<double,ll>>> pq;
+    pq.push({0.0,nn_id});
+    while(!pq.empty()){
+        auto x = pq.top();
+        pq.pop();
+        for(auto y: g.adj[x.second]){
+            if(!visit[y.second]){
+                if(y.first + sp[x.second] < sp[y.second]){
+                    sp[y.second] = y.first + sp[x.second];
+                    pq.push({sp[y.second], y.second});
+                }
+            }
+        }
+        visit[x.second]=true;
+    }
+    std::vector<std::pair<double,ll>>ans;
+    for(int i=0;i<g.adj.size();i++){
+        auto n = g.nodes[i];
+        if(sp[i] != __LONG_LONG_MAX__ && std::find(n.pois.begin(), n.pois.end(),poi) != n.pois.end()){
+            ans.push_back({sp[i],n.id});
+        }
+    }
+    std::sort(ans.begin(),ans.end());
+    std::vector<ll>final;
+    int x = (ans.size()<k)? ans.size() : k;
+    for(int i=0;i<x;i++){
+        final.push_back(ans[i].second);
+    }
+    return final;
+}
---
+++ Phase1/knn.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "graph.hpp"
+#include<vector>
+#include<string>
+#include<cmath>
+#include<queue>
+#include<algorithm>
+std::vector<ll> euclidean(const Graph &g,ll id, std::string poi, ll k, double lat, double lon);
+std::vector<ll>shortest_path(const Graph &g,ll id, std::string poi, ll k, double lat, double lon);
+ll nn(const Graph &g, double lat, double lon);
